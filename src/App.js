17b4918017afcb9cf2eef59b0b4bed504b8d8df7
Jens Christiansen <jens.christiansen@bchydro.com>
2020-02-16 23:37:57 -0800

    add router with high scores page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./App.css";
 import Header from "./components/Header";
 import TypingTest from "./pages/TypingTest";
+import { Switch, Route } from "react-router-dom";
+import HighScore from "./pages/HighScore";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <TypingTest />
+
+      <Switch>
+        <Route exact path="/">
+          <TypingTest />
+        </Route>
+        <Route path="/scores">
+          <HighScore />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function() {
-  return <h1>How Fast Can You Type?</h1>;
+  return (
+    <header>
+      <h1>How Fast Can You Type?</h1>
+      <nav>
+        <Link to="/">Take The Test</Link>
+        <Link to="/scores">High Scores</Link>
+      </nav>
+    </header>
+  );
 }
