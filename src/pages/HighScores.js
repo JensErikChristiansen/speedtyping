dc0a742afd6f11f9fb21af2f1b7fcf79b3594e71
Jens Christiansen <jens.christiansen@bchydro.com>
2020-02-17 23:52:11 -0800

    add highscores from localStorage to HighScores page

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,22 @@
 import React from "react";
+import createLocalStorage from "../util/localStorage";
+import { LOCAL_STORAGE } from "../constants";
+
+const { HIGH_SCORES } = LOCAL_STORAGE;
 
 export default function() {
+  const storage = createLocalStorage(HIGH_SCORES, []);
+
   return (
     <>
       <h1>High Scores</h1>
+      {storage
+        .get()
+        .sort()
+        .reverse()
+        .map((score, idx) => (
+          <h2 key={idx}>{score}</h2>
+        ))}
     </>
   );
 }
